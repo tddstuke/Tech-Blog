6b67e6fa975844a977accb98ce4575a638b4c935
Todd Stuke <tddstuke@gmail.com>
2022-06-18 15:39:53 -0500

    added session and handlebars vars to session.js

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const routes = require("./controllers");
+const helpers = require("./utils/helper");
 
 const sequelize = require("./config/connection");
+
+const path = require("path");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+
+const session = require("express-session");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUnititioalized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 const app = express();
 
