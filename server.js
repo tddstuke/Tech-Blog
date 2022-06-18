d0eb3171590e48f43c85eb681c59bda4deabdf1d
Todd Stuke <tddstuke@gmail.com>
2022-06-18 16:32:42 -0500

    handbars basic html started

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./controllers");
 const helpers = require("./utils/helper");
+require("dotenv").config();
 
 const sequelize = require("./config/connection");
 
@@ -16,7 +17,7 @@ const sess = {
   secret: process.env.SESSION_SECRET,
   cookie: {},
   resave: false,
-  saveUnititioalized: true,
+  saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize,
   }),
@@ -26,8 +27,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
 
 app.use(routes);
 
