e540d996e989d7768fd3d9999a6a002f51e95d6f
Todd Stuke <tddstuke@gmail.com>
2022-06-18 20:26:29 -0500

    homepage base template set up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,8 +2,18 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
-router.get("/", (req, res) => {
-  res.render("homepage");
+router.get("/", async (req, res) => {
+  try {
+    const data = Post.findAll({
+      attributes: ["title", "post_content", "created_at"],
+      include: { model: User, attributes: ["username"] },
+    });
+    const posts = (await data).map((post) => post.get({ plain: true }));
+    res.render("homepage", { posts });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
