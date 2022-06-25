28c0ede1631527390e2a7c9c80d29b19aea705ac
Todd Stuke <tddstuke@gmail.com>
2022-06-25 08:37:23 -0500

    error work

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: "secret",
+  secret: process.env.SESSION_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
